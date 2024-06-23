03f1aa047e595869125483f70ade693a1878d428
stmm-ak <111201911+stmm-ak@users.noreply.github.com>
2024-06-22 20:11:25 -0700

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = { firstName: 'Alex', lastName: 'Klykov', email: 'alex@horizon.com'};
   return (
-    <div>page</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently"
+                />
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={12504.35}
+                />
+            </header>
+
+            recent transactions
+        </div>
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{ currentBalance: 333.99 },{ currentBalance: 3420.99 }]}
+        />
+    </section>
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,17 @@
+"use client"
+import CountUp from "react-countup"
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div>
+        <CountUp 
+            decimal="."
+            prefix="$"
+            duration={1.2}
+            decimals={2}
+            end={amount} />
+    </div>
+  )
+}
+
+export default AnimatedCounter
