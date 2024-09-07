654913f972403eeddbc4f872da3c851567a8d21c
stmm-ak <111201911+stmm-ak@users.noreply.github.com>
2024-09-07 09:14:13 -0700

    Update plaid config

--- lib/plaid.ts
+++ lib/plaid.ts
@@ -2,7 +2,7 @@
 import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
 
 const configuration = new Configuration({
-    basePath: PlaidEnvironments.sandbox,
+    basePath: PlaidEnvironments.production,
     baseOptions: {
         headers: {
             'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
